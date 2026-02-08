0c88582f9acf7afba3b4e9e306f3563c70d4b34f
kimberleyjthornton-debug <kimberleyjthornton@gmail.com>
2026-02-08 16:16:13 +0000

    Form with basic Javascript form/submit function applied with typewriter plugin to amend innerHTML

--- src/script.js
+++ src/script.js
@@ -1,1 +1,14 @@
-alert("Hello1! Welcome to the Motivational Quote Generator!");
+function generateQuote(event) {
+  event.preventDefault();
+
+  new Typewriter("#quote-text", {
+    strings:
+      "The only way to do great work is to love what you do. - Steve Jobs",
+    autoStart: true,
+    cursor: "",
+    delay: 1,
+  });
+}
+
+let quoteForm = document.querySelector("#quote-form");
+quoteForm.addEventListener("submit", generateQuote);

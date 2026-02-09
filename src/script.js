be4310f37b161031d5f0f108434e5de46c00e3f6
kimberleyjthornton-debug <kimberleyjthornton@gmail.com>
2026-02-09 11:26:20 +0000

    Hid the quote container until quote display and added "generating quote" feature (plus JS clean up)

--- src/script.js
+++ src/script.js
@@ -10,16 +10,18 @@ function displayQuote(response) {
 function generateQuote(event) {
   event.preventDefault();
 
-  let userPrompt = document.querySelector("#user-prompt").value;
+  let userPrompt = document.querySelector("#user-prompt");
   let apiKey = "o3d03f027be48ad05b5ec0c715cadt7d";
-  let prompt = `Please generate a motivational quote relating to ${userPrompt}`;
+  let prompt = `Please generate a motivational quote relating to ${userPrompt.value}`;
   let context =
     "You are a knowledeable AI that generates inspirational and uplifting motivational quotes. The quote should be short, impactful, and suitable for sharing on social media. Please include emojis to make the quote fun. The quote should be in English and should not exceed 20 words. The quote should encourage people to stay motivated, be prepared to overcome challenges and endure hardships.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  console.log("Generating poem...");
-  console.log(`Prompt: ${prompt}`);
-  console.log(`Context: ${context}`);
+  let unhideQuoteContainer = document.querySelector(".quote-container");
+  unhideQuoteContainer.style.display = "block";
+
+  let quoteGenerating = document.querySelector("#quote-text");
+  quoteGenerating.innerHTML = `<div class="generating"><small>⏳ Generating a motivational quote about ${userPrompt.value}...</small></div>`;
 
   axios.get(apiUrl).then(displayQuote);
 }
